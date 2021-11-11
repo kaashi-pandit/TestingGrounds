6825ecf1cd314dc699d99459433f3ee4c02b1e58
piyushpandey1992 <piyushphodu@gmail.com>
2021-11-11 21:24:23 +0800

    244 lecture

--- Source/TestingGrounds/TestingGrounds.Build.cs
+++ Source/TestingGrounds/TestingGrounds.Build.cs
@@ -8,6 +8,15 @@ public class TestingGrounds : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"AIModule",
+			"GameplayTasks"
+		});
 	}
 }
